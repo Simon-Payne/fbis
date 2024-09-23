e7bab66ccf6308c0fc3dbf4acc7965f99c1a6f95
Simon Payne <spayne2012@gmail.com>
2024-09-23 15:12:11 +0100

    Add integration test to verify receipt of subscribed websocket push notifications

--- service/src/main/java/com/flatshire/fbis/components/WebSocketConfig.java
+++ service/src/main/java/com/flatshire/fbis/components/WebSocketConfig.java
@@ -1,14 +1,27 @@
 package com.flatshire.fbis.components;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ExecutorChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.nio.charset.StandardCharsets;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private static final Logger log = LoggerFactory.getLogger(WebSocketConfig.class);
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -21,4 +34,28 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/bus-location-feed");
     }
 
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new LoggingChannelInterceptor());
+    }
+
+    private static class LoggingChannelInterceptor implements ExecutorChannelInterceptor {
+        @Override
+        public org.springframework.messaging.Message<?> preSend(Message<?> message, MessageChannel channel) {
+            StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+            StompCommand command = accessor.getCommand();
+            assert command != null;
+            SimpMessageType messageType = command.getMessageType();
+            if (messageType.equals(SimpMessageType.SUBSCRIBE) && message.getHeaders().containsKey("simpDestination")) {
+                String destination = (String) message.getHeaders().get("simpDestination");
+                log.info("Intercepted message type %s with destination %s".formatted(messageType.name(), destination));
+            } else if (messageType.equals(SimpMessageType.MESSAGE)) {
+                byte[] payload = (byte[]) message.getPayload();
+                log.info("Message payload: %s".formatted(new String(payload, StandardCharsets.UTF_8)));
+            } else {
+                log.info("Intercepted message type %s".formatted(messageType.name()));
+            }
+            return message;
+        }
+    }
 }
