95fa979cc5df8466b91833eca37d0122caa7d729
Simon Payne <4488913+Simon-Payne@users.noreply.github.com>
2025-05-14 11:57:27 +0100

    Move websocket service operations into new FbisService component to enable better testability

---
+++ service/src/main/java/com/flatshire/fbis/components/FbisService.java
@@ -0,0 +1,55 @@
+package com.flatshire.fbis.components;
+
+import com.flatshire.fbis.messages.BusPositionResponse;
+import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class FbisService {
+
+    private static final Logger log = LoggerFactory.getLogger(FbisService.class);
+
+    private final BodsServiceImpl bodsService;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    public FbisService(BodsServiceImpl bodsService, SimpMessagingTemplate messagingTemplate) {
+        this.bodsService = bodsService;
+        this.messagingTemplate = copyMessagingTemplate(messagingTemplate);
+    }
+
+    public Pair<String, String> readFeedForLineRef(String lineRef) {
+        Pair<String, String> dataFeed = bodsService.readPositionFromDataFeed(lineRef);
+        log.info("Datafeed bus {} - {}:{}",
+                lineRef,
+                dataFeed.getLeft(),
+                dataFeed.getRight());
+        return dataFeed;
+    }
+
+    public void pushUpdateToLineRef(String lineRef, Pair<String, String> coordinates) {
+            log.info("Map update bus {} - {}:{}",
+                    lineRef,
+                    coordinates.getLeft(),
+                    coordinates.getRight());
+            messagingTemplate.convertAndSend("/topic/buspos/%s/".formatted(lineRef),
+                    new BusPositionResponse(lineRef,
+                            coordinates.getLeft(),
+                            coordinates.getRight()));
+    }
+
+    private static SimpMessagingTemplate copyMessagingTemplate(SimpMessagingTemplate messagingTemplate) {
+        SimpMessagingTemplate copy = new SimpMessagingTemplate(messagingTemplate.getMessageChannel());
+        copy.setHeaderInitializer(messagingTemplate.getHeaderInitializer());
+        copy.setSendTimeout(messagingTemplate.getSendTimeout());
+        copy.setUserDestinationPrefix(messagingTemplate.getUserDestinationPrefix());
+        copy.setDefaultDestination(messagingTemplate.getDefaultDestination());
+        copy.setMessageConverter(messagingTemplate.getMessageConverter());
+        return copy;
+    }
+}
